cb6ca64f757c6b89ee5424f00cb3e7f3f73bfd35
Stayzie-R <Rosova.Stazka@gmail.com>
2025-04-26 12:12:51 +0200

    logging received sensor

--- app.py
+++ app.py
@@ -74,6 +74,10 @@ def update_vector():
         }
         for sensor in data["sensors"]
     ]
+
+    for sensor in self.sensors:
+        print("SENSOR: ", sensor["color"], ", ", sensor["vector"], ", ", sensor["value"])
+        
     graph.on_update(light_vector, sensors)
 
     return flask.Response("""
