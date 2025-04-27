60176d0a58bb79ae929a39ff524796481bbadf8d
Stayzie-R <Rosova.Stazka@gmail.com>
2025-04-27 04:10:01 +0200

    try to repair callbacks

--- custom.js
+++
@@ -1,5 +0,0 @@
-function updateStoreWithLightVector(data) {
-    let light_vector = data.light_vector;
-    let store = document.getElementById('light-vector-store');
-    store.data = light_vector;  // Nastavíme novou hodnotu do Store
-}
