6c2be8d07dab3f490bd8e0f996b8d519950d3152
Stayzie-R <Rosova.Stazka@gmail.com>
2025-04-27 05:29:51 +0200

    try to repair callbacks

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,9 @@
+console.log("Testovací zpráva: Skript je načten");
+
 const eventSource = new EventSource('/update_vector');
 eventSource.onmessage = function(event) {
     console.log('Přijata zpráva:', event.data);
-    
+
     const storeElement = document.getElementById('light-vector-store');
     storeElement.data = storeElement.data === true ? false : true;
     console.log('storeElement data změněna na:', storeElement.data);
