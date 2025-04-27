c15319e32699cc947a01cac9c71f614d229a96b7
Stayzie-R <Rosova.Stazka@gmail.com>
2025-04-27 11:13:56 +0200

    try to repair callbacks

--- app.py
+++ app.py
@@ -67,12 +67,10 @@ def update_vector():
 @app.callback(Output('3d-graph', 'figure'),
               [Input('interval-component', 'n_intervals')])
 def update_plot(n_intervals):
-    print("---- INTERVAL --- ")
     if app.layout['data-store'].data['update']:
         app.layout['data-store'].data['update'] = False
         return graph.update_light_vec()
     else:
-        
         raise PreventUpdate
 
 
