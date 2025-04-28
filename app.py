e46cd5568b1f39e8b620b104830dade45e8bc6d7
Stayzie-R <Rosova.Stazka@gmail.com>
2025-04-28 14:44:24 +0200

    second graph configuration

--- app.py
+++ app.py
@@ -38,6 +38,17 @@ app.layout = html.Div(
                 'modeBarButtonsToRemove': ['zoom2d', 'pan2d', 'select2d', 'lasso2d']
             }
         ),
+        dcc.Graph(
+            id="2d-graph",
+            figure=graph.figure_2d,  
+            style={
+                "margin": "auto",
+                "display": "block"
+            },
+            config={
+                'modeBarButtonsToRemove': ['zoom3d', 'pan3d', 'select3d', 'lasso3d', 'resetCameraLastSave3d', 'resetCameraDefault3d']
+            }
+        ),
         dcc.Interval(id='interval-component',interval=1000),
         dcc.Store(id='data-store', data={'update': False})
     ],
