97ecb33a9c8a0ee3ddbf194fde5ca0ad099e13e3
Stayzie-R <Rosova.Stazka@gmail.com>
2025-04-29 20:01:09 +0200

    debugging -> testing files

---
+++ test.py
@@ -0,0 +1,48 @@
+
+import os
+
+import dash
+from dash import dcc, html
+from TestGraph import TestGraph
+import flask
+from flask import jsonify
+
+import logging
+logging.getLogger('werkzeug').setLevel(logging.ERROR)
+
+server = flask.Flask(__name__)
+secret_key = os.environ.get("SECRET_KEY", "secret")
+app = dash.Dash(
+    __name__,
+    server=server,
+    update_title="",
+)
+graph = TestGraph()
+
+app.title = "ADCS - Visualization"
+app.layout = html.Div(
+    children=[
+        dcc.Graph(
+            id="graph",
+            figure=graph.fig,
+        ),
+        dcc.Interval(id='interval-component',interval=1000),
+    ],
+)
+
+@app.server.route('/test', methods=['POST'])
+def update_vector():
+    data = request.get_json()
+    light_vector = data["light_vector"]
+    print("received vector: ", str(graph.light_vector))
+    return jsonify({"status": "success", "message": "Data received and processed"})
+
+@app.callback(Output('graph', 'figure'),
+              [Input('interval-component', 'n_intervals'))
+def update_plot(n_intervals, data_store):
+    print("updating vector: ", str(graph.light_vector))
+    return graph.fig
+
+
+if __name__ == "__main__":
+    app.run(debug=True,use_reloader=False)
