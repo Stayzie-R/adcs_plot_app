9324d1a30418e4a2d3efe78c0bd718ad2f94fe5a
Stayzie-R <Rosova.Stazka@gmail.com>
2025-04-30 18:47:00 +0200

    Comment with a block - camera position callback

--- app.py
+++ app.py
@@ -96,13 +96,14 @@ def update_vector():
 
 camera_move_lock = False
 
-@app.callback(
-     Output('camera-output', 'children'), Input('3d-graph', 'relayoutData'))
-def update_camera(relayout_data):
-    if relayout_data and 'scene.camera' in relayout_data:
-         camera = relayout_data['scene.camera']['eye']
-         return f"Camera position:\nx: {camera['x']:.2f}, y: {camera['y']:.2f}, z: {camera['z']:.2f}"
-    return "Camera position: not moved yet"
+# @app.callback(
+#      Output('camera-output', 'children'), Input('3d-graph', 'relayoutData'))
+# def update_camera(relayout_data):
+#     if relayout_data and 'scene.camera' in relayout_data:
+#          camera = relayout_data['scene.camera']['eye']
+#          #return f"Camera position:\nx: {camera['x']:.2f}, y: {camera['y']:.2f}, z: {camera['z']:.2f}"
+#     return "Camera position: not moved yet"
+
 
 @app.server.route('/interaction_start', methods=['POST'])
 def lock_camera_backend():
